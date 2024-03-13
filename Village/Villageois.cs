5f6152ab595a8ad32bf88db32f6ad354c663140a
mimouni_a <mimouni_a@D202-022024-06>
2024-03-13 10:59:34 +0100

    Ajoutez des fichiers projet.

---
+++ Village/Villageois.cs
@@ -0,0 +1,71 @@
+﻿using System.ComponentModel.Design;
+
+namespace Village
+{
+    public class Villageois
+    {
+        private bool _malade;
+        private string _nom;
+        private int _poids;
+
+        public Villageois(bool malade, string nom, int poids)
+        {
+            this._malade = malade;
+            this._nom = nom;    
+            this._poids = poids;
+ 
+        }
+
+        public Villageois(string nom)
+        {
+            this._nom = nom;
+            bool _malade = false;
+
+        }
+
+        public bool get_Malade() 
+        {
+            return this._malade;
+        }
+
+        public string get_Nom()
+        {
+            return this._nom;
+        }
+
+        public int get_Poids()
+        {
+            return this._poids;
+        }
+
+        public override string ToString() 
+        {
+            return "Le villageois se nomme " + this._nom + " et peut soulever " + this._poids + " et est " + this._malade;
+        }
+
+        public int poidsSouleve()
+        {
+            int resultat = 0;
+            resultat = _poids / 5;
+
+            return resultat;
+        }
+
+        public int setPoids(int poids) 
+        {
+            _poids = poids;
+            return _poids ;
+        }
+
+        public bool soulevememepoids(Villageois a)
+        {
+            if (this._poids == a.get_Poids())
+            {
+                return true;
+            }
+            else return false;
+        }
+
+
+    }
+}
---
+++ Villageois/Program.cs
@@ -0,0 +1,23 @@
+﻿using Village;
+
+Villageois a, b;
+string rep;
+
+a = new Villageois(true, "Panoramix", 16);
+b = new Villageois("Abraracourcix");
+Console.WriteLine(b.get_Poids());
+b.setPoids(26);
+Console.WriteLine(a.ToString());
+Console.WriteLine(a.get_Nom());
+Console.WriteLine(a.get_Poids());
+Console.WriteLine(a.poidsSouleve() + "Kg");
+Console.WriteLine(a.soulevememepoids(b));
+
+if (a.poidsSouleve() > b.poidsSouleve())
+    {
+    Console.WriteLine(a.get_Nom() + " soulève plus lourd que " + b.get_Nom());
+    }
+else
+{
+    Console.WriteLine(b.get_Nom() + " soulève plus lourd que " + a.get_Nom());
+}
